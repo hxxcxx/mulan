a05edcdf21f30ad7ceb0eeedf976618abbba2b1d
hxxcxx <hxxcxx@126.com>
2026-07-02 01:49:20 +0800

    fix(rhi): 设备创建失败时改用引擎层错误码 DeviceLost

--- src/engine/rhi/device_factory.cpp
+++ src/engine/rhi/device_factory.cpp
@@ -1,6 +1,7 @@
 #include "device.h"
 #include "vulkan/vk_device.h"
 #include "d3d12/dx12_device.h"
+#include "../engine_error_code.h"
 
 #include <mulan/core/result/error.h>
 
@@ -21,8 +22,7 @@ std::expected<std::shared_ptr<RHIDevice>, core::Error> RHIDevice::create(const D
                                   "Graphics backend not implemented"));
         }
     } catch (const std::exception& e) {
-        return std::unexpected(
-            core::Error::make(core::ErrorCode::Internal, e.what()));
+        return std::unexpected(makeError(EngineErrorCode::DeviceLost, e.what()));
     }
 }
 
